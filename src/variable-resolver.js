10dc97885229a995cb1b3ec3a34f57fa7451bb6f
Akash Goswami <akash@fusioncharts.com>
2018-09-11 17:42:25 +0530

    Improve readability of docs

--- src/variable-resolver.js
+++ src/variable-resolver.js
@@ -1,18 +1,17 @@
 /**
- * A format of specifying resolve criterial of a new variable from existing variable. The variables could be Measure or
- * Dimension.
- * The format for resolver is intuitive. It needs all the existing variable which is dependency of the new variable
- * listed adjacently followed by the resolving function in a single array.
+ * A format of specifying resolve criteria of a new variable from existing variables. Newly created variables could be a
+ * measure or dimension.
+ * The format for resolver is intuitive. It needs name of all existing variables, using which the value of new variable
+ * is calculated, listed adjacently followed by the resolving function in a single array.
  *
- * The resolver function gets called for each row of data model passing the {@link Value} of dependent variables as
+ * The resolver function gets called for each row of data passing the {@link Value} of dependent variables as
  * parameter
  * ```
  *  ['horsepower', 'acceleration', (horsepower, acceleration) => {
- *      return acceleration / horsepower
+ *      return acceleration / horsepower;
  *  }]
  * ```
  *
  * @public
  * @module VariableResolver
- *
  */
