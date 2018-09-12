8b584a2fc4dfe86dec26e2fcbb4cc0c774646077
Akash Goswami <akash@fusioncharts.com>
2018-09-12 15:32:49 +0530

    Change location of FilteringMode doc

--- src/enums/filtering-mode.js
+++ src/enums/filtering-mode.js
@@ -1,8 +1,10 @@
 /**
- * Filtering mode enum defines the filering modes of DataModel.
+ * FilteringMode determines if resultant DataModel should be created from selection set or rejection set.
  *
- * @readonly
- * @enum {string}
+ * The following modes are available
+ * - `NORMAL`: Only entries from selection set are included in the resulatant DataModel instance
+ * - `INVERSE`: Only entries from rejection set are included in the resulatant DataModel instance
+ * - ALL: Both the entries from selection and rejection set are returned in two different DataModel instance
  */
 const FilteringMode = {
     NORMAL: 'normal',
