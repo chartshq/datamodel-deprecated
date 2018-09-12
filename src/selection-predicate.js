e638452f24f2e7569b5b54c8832cbb8bc437098f
Akash Goswami <akash@fusioncharts.com>
2018-09-12 15:33:45 +0530

    Improve readability of doc

--- src/selection-predicate.js
+++ src/selection-predicate.js
@@ -1,27 +1,31 @@
 /**
- * A simple predicate which decides if a row is selected for a given condition or not. If this predicate returns true
+ * A simple predicate which decides if a row is selected based on some condition or not. If this predicate returns true
  * the current row is labeled as a member of selection set. Similarly if returns false, the current row is labeled as
  * a memeber of rejection set.
  *
- * Note: This funciton does not directly decides whether the row should be present in the final resultant DataModel.
- * Based on selection set and rejection set which predicate function helps to decide, MODE determine which row should
- * be present in the final DataModel.
+ * Note: For some cases this funciton does not directly decides whether the row should be present in the final resultant
+ * DataModel. Based on selection set and rejection set which predicate function helps to decide, MODE determine which
+ * set should be present in the final DataModel.
  *
  * @public
  * @module SelecitonPredicate
  *
- * @param {object} rowInf Particular row information presented as object.
+ * @param {...object} rowInf Particular row information presented as object.
  *      When rows are iterated by a operator, all the fields {@link Value | value} present in a row, are combined in a
  *      object. The key of the object is name of field and value is {@link Value | value} of field for that current row.
  *      A typical value of a row is represented by
  *
  *      ```
  *      {
- *          origin: Value,
- *          horsepower: Value
+ *          Origin: Value,
+ *          Horsepower: Value
  *          ...
  *      }
  *      ```
+ *      Number of arguments varies based on no if operands are involved for a operator. Like, for joining two instances
+ *      of DataModels are involved, hence two rowInf are passed, one for each DataModel instance. On
+ *      the other hand, operators like selection needs only one operands, hence only one rowInf is
+ *      passed.
  *
  * @param {Number} rowIndex Index of current iteration of row
  */
